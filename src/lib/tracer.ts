a515daed50ec053344d2508f2dc9779b546e7a60
Ajibola Oluwatobi <emmanuelloluwatobi@gmail.com>
2026-02-02 14:09:07 +0100

    init..

---
+++ src/lib/tracer.ts
@@ -0,0 +1,11 @@
+// Datadog tracer initialization
+// This module initializes Datadog APM tracing for production environments
+
+export default function initTracer() {
+  // Datadog tracer initialization will be implemented here
+  // For now, this is a placeholder to prevent import errors
+  if (typeof window === "undefined") {
+    // Server-side initialization
+    // TODO: Initialize Datadog tracer when needed
+  }
+}
